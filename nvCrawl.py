aecc477da54cc1aa683e46e0a4bf93fb75eab296
psoon <bestkcs1234@naver.com>
2022-05-11 08:41:39 +0900

    Initial commit

---
+++ nvCrawl.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import urllib.request
+import datetime
+import time
+import json
+import pandas as pd
+
+client_id = '57C14kuyVcTLyqtDsbLG'
+client_secret = 'HzuDGrbb5J'
+
+
+#[CODE 1]
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print ("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+
+#[CODE 2]
+def getNaverSearch(node, srcText, start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), start, display)
+
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)   #[CODE 1]
+
+    if (responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+
+#[CODE 3]
+def getPostData(post, jsonResult, cnt): #lastBuildDate:
+    title = post['title']
+    description = post['description']
+    # org_link = post['originallink']
+    link = post['link']
+    bloggername = post['bloggername']
+    # pDate = datetime.datetime.strptime(post['postdate'], '%a, %d %b %Y %H:%M:%S +0900') # lastBuildDate / post['pubDate']
+    # pDate = pDate.strftime('%Y-%m-%d %H:%M:%S')
+    pDate = datetime.datetime.strptime(post['postdate'], '%Y%m%d') # lastBuildDate / post['pubDate']
+    pDate = pDate.strftime('%Y%m%d')
+
+    jsonResult.append({'cnt':cnt, 'title':title, 'description': description,
+                       # 'org_link':org_link,
+                       'link': link,   'pDate':pDate
+                          , 'bloggername':bloggername})
+
+
+    return
+
+#[CODE 0]
+def main():
+    node = 'blog'   # 크롤링 할 대상
+    srcText = input('검색어를 입력하세요: ')
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)  #[CODE 2]
+    total = jsonResponse['total']
+
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']:
+            cnt += 1
+            getPostData(post, jsonResult, cnt)  #[CODE 3] # , jsonResponse['lastBuildDate']
+
+        start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100)  #[CODE 2]
+
+    print('전체 검색 : %d 건' %total)
+
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
+        jsonFile = json.dumps(jsonResult,  indent=4, sort_keys=True,  ensure_ascii=False)
+
+        outfile.write(jsonFile)
+
+    print("가져온 데이터 : %d 건" %(cnt))
+    print ('%s_naver_%s.json SAVED' % (srcText, node))
+
+    df = pd.DataFrame(jsonResult)
+    df = df.set_index('cnt')
+    df.to_csv('%s_naver_%s.csv' % (srcText, node), encoding='utf-8-sig')
+
+if __name__ == '__main__':
+    main()
